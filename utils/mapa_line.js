201c160284791717e382926858cb6a50791f41a9
Henrique Furst Scheid <hfscheid@gmail.com>
2023-06-18 17:48:56 -0300

    feat: summing up all yearly data and creating year-count array

--- utils/mapa_line.js
+++ utils/mapa_line.js
@@ -22,14 +22,18 @@ function formatYearlyData(data) {
     } else {
       yearlydata[item.year] = item.count;
     }
-  );
-  return yearlydata;
+  });
+  const yearlyarray = [];
+  for (year of yearlydata) {
+    yearlyarray.push({"year": year, "count": yearlydata[year]});
+  }
+  return yearlyarray;
 }
 //Read the data
 async function getLineData(state = 0) {
   const response = await fetch(`/data/line/${evaluation.value}/${knowledge.value}/${research.value}/${level.value}/${startYear.value}/${endYear.value}/${state}`);
   const data = await response.json();
-	console.log('line data:\n\n');
+    console.log('line data:\n\n');
   console.log(data);
   return formatYearlyData(data);
 }
@@ -47,19 +51,19 @@ function drawLineChart(id, data) {
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
   // Add X axis --> it is a date format
-	var x = d3.scaleTime()
+    var x = d3.scaleTime()
     .domain(d3.extent(data, function(d) { return d.year; }))
     .range([ 0, width ]);
-	svg.append("g")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x).tickFormat(d3.format("d")));
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
-	// Add Y axis
-	var y = d3.scaleLinear()
-		.domain([0, d3.max(data, function(d) { return +d.count; })])
-		.range([ height, 0 ]);
-	svg.append("g")
-		.call(d3.axisLeft(y));
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return +d.count; })])
+        .range([ height, 0 ]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
   // title
   svg.append("text")
@@ -70,33 +74,33 @@ function drawLineChart(id, data) {
     .style("font-weight", "bold")
     .text("Total de publicações por ano");
 
-	// Add total data line
-	svg.append("path")
-		.datum(data)
-		.attr("fill", "none")
-		.attr("stroke", "steelblue")
-		.attr("stroke-width", 1.5)
-		.attr("d", d3.line()
-			.x(function(d) { return x(d.year) })
-			.y(function(d) { return y(d.count) })
-			)   
+    // Add total data line
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(function(d) { return x(d.year) })
+            .y(function(d) { return y(d.count) })
+            )   
 
-	// Draw the point bullets
-	svg.selectAll(".point")
-	.data(data)
-	.enter()
-	.append("circle")
-	.attr("class", "point")
-	.attr("cx", d => x(d.year))
-	.attr("cy", d => y(d.count))
-	.attr("r", 4)
-	.on("mouseover", mouseover)
+    // Draw the point bullets
+    svg.selectAll(".point")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "point")
+    .attr("cx", d => x(d.year))
+    .attr("cy", d => y(d.count))
+    .attr("r", 4)
+    .on("mouseover", mouseover)
   .on("mousemove", mousemove)
-	.on("mouseout", mouseleave);
+    .on("mouseout", mouseleave);
 
-	// Function to show the tooltip
+    // Function to show the tooltip
 
-	var Tooltip = d3.select(id)
+    var Tooltip = d3.select(id)
     .append("div")
     .style("opacity", 0)
     .attr("class", "tooltip")
@@ -114,67 +118,67 @@ function drawLineChart(id, data) {
       .style("left", (d3.pointer(this)[0]+70) + "px")
       .style("top", (d3.pointer(this)[1]) + "px")
   }
-	function mouseover(d) {
-		Tooltip.style("opacity", 1);
+    function mouseover(d) {
+        Tooltip.style("opacity", 1);
 
-		d3.select(this)
-		  .style("stroke", "black")
-		  .style("opacity", 1);
-	  
-		const dataD = d.toElement.__data__;
-	  
-		var value = dataD.count;
-	  
-		if (relative_line.checked) {
-		  value = (value * 100).toFixed(2);
-		}
-	  
-		Tooltip
-		  .html("Count: " + value)
-		  .style("left", (d3.pointer(this)[0] + 70) + "px")
-		  .style("top", (d3.pointer(this)[1] + margin.top + 10) + "px");
-	}
+        d3.select(this)
+          .style("stroke", "black")
+          .style("opacity", 1);
+      
+        const dataD = d.toElement.__data__;
+      
+        var value = dataD.count;
+      
+        if (relative_line.checked) {
+          value = (value * 100).toFixed(2);
+        }
+      
+        Tooltip
+          .html("Count: " + value)
+          .style("left", (d3.pointer(this)[0] + 70) + "px")
+          .style("top", (d3.pointer(this)[1] + margin.top + 10) + "px");
+    }
 
-	function mouseleave() {
-		Tooltip
-		.style("opacity", 0)
-	  d3.select(this)
-		.style("stroke", "none")
-		.style("opacity", 0.8)
-	}
+    function mouseleave() {
+        Tooltip
+        .style("opacity", 0)
+      d3.select(this)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+    }
 
 }
 
 async function update(){
-	const data = await getLineData();
+    const data = await getLineData();
 
-	if(relative_line.checked){
-	var pre = 0
-	for(let i = 0; i<data.length; i++){
-	  if(pre == 0){
-		  pre = data[i].count
-		  data[i].count = 0
-	  }
-	  else{
-	  var s = data[i].count
-	  data[i].count = s/pre
-	  pre = s}
+    if(relative_line.checked){
+    var pre = 0
+    for(let i = 0; i<data.length; i++){
+      if(pre == 0){
+          pre = data[i].count
+          data[i].count = 0
+      }
+      else{
+      var s = data[i].count
+      data[i].count = s/pre
+      pre = s}
   
-	}}
-	console.log(data.length)
-	console.log(data[0])
-	console.log(data)
-	drawLineChart("#line_chart", data);
+    }}
+    console.log(data.length)
+    console.log(data[0])
+    console.log(data)
+    drawLineChart("#line_chart", data);
 }
 
 const relative_line = document.getElementById("relative_line")
 
 search.addEventListener("click", async () => {
-	update()
+    update()
 });
 
 relative_line.addEventListener("click", async () => {
-	update()
+    update()
 });
 
 export {getLineData, drawLineChart};
